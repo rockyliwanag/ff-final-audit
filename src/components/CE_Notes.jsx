33b05634f9f1c9f019d17cbb0d2884505aae95ed
Rocky Liwanag <studiorockprod@me.com>
2024-06-18 11:33:01 -0700

    feat: Add ProjDetails and CE_Notes components
    The code changes include adding the ProjDetails component for displaying project details and the CE_Notes component for capturing sales information.

--- src/components/CE_Notes.jsx
+++ src/components/CE_Notes.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from "react";
-import { Input, Radio, Select, Option } from '@material-tailwind/react';
+import { Input, Radio, Select, Option, Typography, Textarea } from '@material-tailwind/react';
 import Icon from '@/assets/Icon';
 import tieIn from '@/app/options/tieIn';
 import attachmentType from '@/app/options/attachmentType';
@@ -27,21 +27,21 @@ const CE_Notes = () => {
     // }, [tieIn])
   return (
     <>
-        <div  className="">
-            <form>
+        <div className="mt-4">
+            <form className="flex flex-col space-y-2">
                 <div className="flex flex-row justify-between">
-                    <label className="">Sales Met</label>
+                    <Typography variant="h4" color='gray'>Sales Met</Typography>
                     <div className='flex flex-row'>
                         <div>
-                            <Radio className="hover:before:opacity-0" color='green' name='salesMet' label='Yes' icon={<Icon/>}/>
+                            <Radio color='green' name='salesMet' label='Yes' icon={<Icon/>}/>
                         </div>
                         <div>
-                            <Radio className="hover:before:opacity-0" color='green' name='salesMet' label='No' icon={<Icon/>}/>
+                            <Radio color='green' name='salesMet' label='No' icon={<Icon/>}/>
                         </div>
                     </div>
                 </div>
                 <div className='flex flex-row justify-between'>
-                    <label>Holding</label> 
+                    <Typography variant="h4" color='gray'>Holding</Typography> 
                     <div className="flex flex-row">
                         <div>
                             <Radio name='holding' color='green' label='Yes' icon={<Icon/>}/>
@@ -52,17 +52,19 @@ const CE_Notes = () => {
                     </div>
                 </div>
                 <div className='flex flex-row'>
-                    <Input variant='standard' type='number' name='numPlanes' color="green" label='Number of Planes' placeholder="0"/>
+                    <Input  type='number' name='numPlanes' color="green" label='Number of Planes' placeholder="0"/>
                 </div>
-                <div className="flex flex-row">
-                    <label>Tie In</label>
-                    {tieIn.map((item, idx) => {
-                        return (
-                            <div key={idx}>
-                                <Radio name='tieIn' color='green' label={item.method} icon={<Icon/>}/>
-                            </div>
-                        )
-                    })} 
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Tie In</Typography>
+                    <div className="flex flex-row">
+                        {tieIn.map((item, idx) => {
+                            return (
+                                <div key={idx}>
+                                    <Radio name='tieIn' color='green' label={item.method} icon={<Icon/>}/>
+                                </div>
+                            )
+                        })} 
+                    </div>
                 </div>
                 <div className="flex flex-row">
                     <Select color='green' variant='standard' label='Attachment'>
@@ -73,101 +75,107 @@ const CE_Notes = () => {
                         })}
                     </Select>
                 </div>
-                <div className="flex flex-row">
-                    <label>Modules</label>
-                    <Input  variant='standard' className='text-black' type='number' name='modules' placeholder='0'/>
-                    <Select color='green' variant="standard" label='Modules'>
-                        {/* <Option value=''>Select Module</Option> */}
+                <div className="flex flex-row space-x-2">
+                <Typography variant="h4" color='gray'>Module</Typography>
+                    <Input className='text-black' type='number' name='modules' label='Module Qty' placeholder='0'/>
+                    <Select color='green' variant="standard" label='Select Module'>
                         {panels.map((item, idx) => {
                             return (
-                                <Option key={idx}>{item.type}</Option>
+                                <Option key={idx} value={item.type}>{item.type}</Option>
                             )
                         })}
                     </Select>
                 </div>
-                <div className="flex flex-row">
-                    <label>Inverters</label>
-                    <input className='text-black' type='number' name='inverters' placeholder='0'/>
-                    <select className="text-black">
-                        <option value=''>Select Inverter</option>
+                <div className="flex flex-row space-x-2">
+                    <Typography variant="h4" color='gray'>Inverter</Typography>
+                    <Input className='w-10' type='number' name='inverters' label='Inverter Qty' placeholder='0'/>
+                    <Select color="green" variant="standard" label="Select Inverter">
                         {inverters.map((item, idx) => {
-                            {console.log("item", item)}
                             return (
-                                <option key={idx} value={item.type}>{item.type}</option>
+                                <Option key={idx} value={item.type}>{item.type}</Option>
                             )
                         })}
-                    </select>
+                    </Select>
+                </div>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>POI</Typography>
+                    <div className="flex flex-row">
+                        {tieIn.map((item, idx) => {
+                            return (
+                                <div key={idx} className="self-end">
+                                    <Radio name='poi' color='green' label={item.method} icon={<Icon/>}/>
+                                </div>
+                            )
+                        })} 
+                    </div>
+                </div>
+                <div className="flex flex-row justify-between">
+                    <Textarea className='' name='poiNotes' label='POI Notes'/>
                 </div>
                 <div className="flex flex-row">
-                    <label>POI</label>
-                    {tieIn.map((item, idx) => {
-                        return (
-                            <div key={idx}>
-                                <input type='radio' name='poi' value={item.method}/>
-                                <label>{item.method}</label>
-                            </div>
-                        )
-                    })} 
-                </div>
-                <div className="flex flex-row">
-                    <label>POI Notes</label>
-                    <textarea className='text-black' name='poiNotes' placeholder='Enter POI Notes'/>
-                </div>
-                <div className="flex flex-row">
-                    <label>Roof Material</label>
-                    <select className="text-black">
-                        <option value=''>Select Material</option>
+                    <Typography variant="h4" color='gray'>Roof Material</Typography>
+                    <Select color='green' variant='standard' label='Select Material'>
                         {roofMatetial.map((item, idx) => {
                             {console.log("item", item)}
                             return (
-                                <option key={idx} value={item.type}>{item.type}</option>
+                                <Option key={idx} value={item.type}>{item.type}</Option>
                             )
                         })}
-                    </select>
+                    </Select>
                 </div>
-                <div className="flex flex-row">
-                    <label>Roof Work</label>
-                    <div>
-                        <input type='radio' name='roofWork' value='yes'/>
-                        <label>Yes</label>
-                        <input type='radio' name='roofWork' value='no'/>
-                        <label>No</label>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Roof Work</Typography>
+                    <div className='flex flex-row'>
+                        <div>
+                            <Radio color='green' name='roofWork' label='Yes' icon={<Icon/>}/>
+                        </div>
+                        <div>
+                            <Radio color='green' name='roofWork' label='No' icon={<Icon/>}/>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row">
-                    <label>Electrical Work</label>
-                    <div>
-                        <input type='radio' name='electricalWork' value='yes'/>
-                        <label>Yes</label>
-                        <input type='radio' name='electricalWork' value='no'/>
-                        <label>No</label>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Electrical Work</Typography>
+                    <div className='flex flex-row'>
+                        <div>
+                            <Radio color='green' name='electricWork' label='Yes' icon={<Icon/>}/>
+                        </div>
+                        <div>
+                            <Radio color='green' name='electricWork' label='No' icon={<Icon/>}/>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row">
-                    <label>Trench</label>
-                    <div>
-                        <input type='radio' name='trench' value='yes'/>
-                        <label>Yes</label>
-                        <input type='radio' name='trench' value='no'/>
-                        <label>No</label>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Trench</Typography>
+                    <div className='flex flex-row'>
+                        <div>
+                            <Radio color='green' name='trench' label='Yes' icon={<Icon/>}/>
+                        </div>
+                        <div>
+                            <Radio color='green' name='trench' label='No' icon={<Icon/>}/>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row">
-                    <label>Tree Trimming</label>
-                    <div>
-                        <input type='radio' name='treeTrim' value='yes'/>
-                        <label>Yes</label>
-                        <input type='radio' name='treeTrim' value='no'/>
-                        <label>No</label>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Tree Trimming</Typography>
+                    <div className='flex flex-row'>
+                        <div>
+                            <Radio color='green' name='treeTrim' label='Yes' icon={<Icon/>}/>
+                        </div>
+                        <div>
+                            <Radio color='green' name='treeTrim' label='No' icon={<Icon/>}/>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row">
-                    <label>Ground Mount</label>
-                    <div>
-                        <input type='radio' name='groundMount' value='yes'/>
-                        <label>Yes</label>
-                        <input type='radio' name='groundMount' value='no'/>
-                        <label>No</label>
+                <div className="flex flex-row justify-between">
+                    <Typography variant="h4" color='gray'>Ground Mount</Typography>
+                    <div className='flex flex-row'>
+                        <div>
+                            <Radio color='green' name='groundMount' label='Yes' icon={<Icon/>}/>
+                        </div>
+                        <div>
+                            <Radio color='green' name='groundMount' label='No' icon={<Icon/>}/>
+                        </div>
                     </div>
                 </div>
             </form>
