de9b94f53375f4dac24faa5d9f8ad65cfd323b73
Rocky Liwanag <studiorockprod@me.com>
2024-08-15 07:09:47 -0700

    `refactor: Remove unused @nextui-org/input dependency from page.js`

--- src/app/notes/page.js
+++ src/app/notes/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { useState } from 'react';
-import {Textarea} from "@nextui-org/input";
 import InputCP from '../../components/InputCP';
 import Navigate from '../../components/Navigation';
 
