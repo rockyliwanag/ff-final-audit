b475d2ea679e8c56e56fe8e6c8e2d680fecc5da0
Rocky Liwanag <studiorockprod@me.com>
2025-03-27 06:47:44 -0700

    feat: Update InputCP component to use Material-UI icons for copy and paste functionality

--- src/components/InputCP.jsx
+++ src/components/InputCP.jsx
@@ -1,8 +1,8 @@
 'use client';
 import { Input, Button } from "@material-tailwind/react"
 import Image from 'next/image'
-import CopyIcon from './../assets/CopyIcon1.svg'
-import PasteIcon from './../assets/PasteIcon1.svg'
+import CopyIcon from '@mui/icons-material/ContentCopy'
+import PasteIcon from '@mui/icons-material/ContentPaste'
 // import CopyIcon from './public/CopyIcon.svg'
 // import PasteIcon from './public/PasteIcon.svg'
 const InputCP = ({inputValue, setInputValue, inputName, variant }) => {
@@ -30,8 +30,8 @@ const InputCP = ({inputValue, setInputValue, inputName, variant }) => {
                     onChange={(e) => setInputValue(e.target.value)}
                     />
                     <div className="flex flex-row justify-end gap-x-4">
-                        <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={copyToClipboard}>{CopyIcon}</button>
-                        <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={pasteFromClipboard}>{PasteIcon}</button>
+                        <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={copyToClipboard}><CopyIcon /></button>
+                        <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={pasteFromClipboard}><PasteIcon /></button>
                     </div>
                 </div>
             :
@@ -45,8 +45,8 @@ const InputCP = ({inputValue, setInputValue, inputName, variant }) => {
                     label={<div className="text-green-300 font-medium"> {inputName} </div>}
                     onChange={(e) => setInputValue(e.target.value)}
                 />               
-                <Button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={copyToClipboard}><Image src={CopyIcon} width={20} height={20}/></Button>
-                <Button className='bg-transparent hover:bg-green500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={pasteFromClipboard}><Image src={PasteIcon} width={20} height={20}/></Button>
+                <Button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={copyToClipboard}><CopyIcon /></Button>
+                <Button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded' onClick={pasteFromClipboard}><PasteIcon /></Button>
             </div>}
         </div>
     </div>
